5e141cb01db1f0461ed4dc4c5f849d3b5ebd997a
mathek <front@student.agh.edu.pl>
2017-06-09 01:53:40 +0200

    added calculating motion

--- app/WSServer.hs
+++ app/WSServer.hs
@@ -6,7 +6,7 @@
 -- available [here](/example/client.html).  In order to understand this example,
 -- keep the [reference](/reference/) nearby to check out the functions we use.
 
-{-# LANGUAGE OverloadedStrings, FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings, FlexibleInstances, StandaloneDeriving #-}
 module WSServer where
 -- import Data.Monoid (mappend)
 import Data.Maybe
@@ -24,10 +24,12 @@ import qualified Network.WebSockets as WS
 
 data ServerState = ServerState{
   sender::Maybe WS.Connection,
-  receiver::Maybe WS.Connection
+  receiver::Maybe WS.Connection,
+  debugger::Maybe WS.Connection,
+  motionCalcState::CalcMotionState
 }
 initialServerState :: ServerState
-initialServerState = ServerState Nothing Nothing
+initialServerState = ServerState Nothing Nothing Nothing initCalcMotionState
 
 run :: String -> Int -> IO ()
 run host port = do
@@ -82,23 +84,80 @@ application (state) pending = do
         connect = do
           handleEvent state $ \s -> ([], s {sender = Just conn})
           forever $ handleMessage conn state $
-            \m s -> [receiver s |> (fmap $ flip Send m)] |> catMaybes
+            \m s -> let
+              (motion', mcState) = m |> parsePosition |> calcMotion (motionCalcState s)
+              m' = motion' |> show |> pack
+              d = m' `mappend` ";" `mappend` m
+              in (
+                [receiver s |> fmap (flip Send $ m'), debugger s |> fmap (flip Send $ d)]
+                  |> catMaybes,
+                s {motionCalcState = mcState}
+              )
     "receiver" -> flip finally disconnect connect
       where
         disconnect = handleEvent state $ \s -> ([], s {receiver = Nothing})
         connect = do
           handleEvent state $ \s -> ([], s {receiver = Just conn})
-          forever $ handleMessage conn state ((\_ _ -> [])::(Text -> ServerState -> [WSAction]))
+          dumbReceive conn state
+    "debugger" -> flip finally disconnect connect
+      where
+        disconnect = handleEvent state $ \s -> ([], s {debugger = Nothing})
+        connect = do
+          handleEvent state $ \s -> ([], s {debugger = Just conn})
+          dumbReceive conn state
     _ -> return ()
 
+dumbReceive::WS.Connection -> MVar ServerState -> IO ()
+dumbReceive conn state =
+  forever $ handleMessage conn state ((\_ _ -> [])::(Text -> ServerState -> [WSAction]))
 
-      -- case msg of
-      --   "sender" -> do flip finally disconnect connect
-      --     where
-      --       disconnect = do modifyMVar_ state $ \s -> return $ s {sender = Nothing}
-      --       connect = do
-      --         modifyMVar_ state $ \s -> return s {sender = Just conn}
-      --         forever $ do
-      --             m <- (WS.receiveData conn)::(IO Text)
-      --             s <- readMVar state
-      --             receiver s |> mapM_ (\r -> WS.sendTextData r m)
+parsePosition::Text -> [Double]
+parsePosition t = t |> splitOn ";" |> fmap (\x -> read (unpack x) :: Double)
+
+data Motion = Stopped | Running | Jumping
+deriving instance Show Motion
+
+data MotionBorder = Top | Bottom | None deriving Eq
+data MotionScaler = MotionScaler {zeroLvl::Double, lastMotion::Double}
+initMotionScaler::MotionScaler
+initMotionScaler = MotionScaler 0 0
+data CalcMotionState = CalcMotionState {
+  motion::Motion, border::MotionBorder, lastChange::Int, lastx::Double, jumpIgnore::Double, scaler::MotionScaler
+}
+
+initCalcMotionState::CalcMotionState
+initCalcMotionState = CalcMotionState Stopped None 0 0 0 initMotionScaler
+
+-- TODO: split into smaller functions, prevent changing motion state until scaler gets stabilized
+calcMotion::CalcMotionState -> [Double] -> (Motion, CalcMotionState)
+calcMotion (CalcMotionState motion' border' i lx lj scaler'@(MotionScaler zeroLvl' lastMotion')) [x, _y, _z, _a, _b, _g] =
+  case border' of
+    _ | lastMotion' > rescaleAfter ->
+      nextState {motion = Stopped, border = None, scaler = MotionScaler x 0}
+    None | x < bottomLvl -> nextState {motion = Running, border = Bottom}
+    b | b `elem` [Top, Bottom] && (x > topJumpLvl || x < bottomJumpLvl) && lj == 0 ->
+      nextState {motion = Jumping, jumpIgnore = jumpIgnore'}
+    Top | x > topLvl -> nextState {motion = Running}
+    Top | x < bottomLvl -> nextState {motion = Running, border = Bottom}
+    Top | lx - x >= minDiff -> nextState {motion = Running}
+    Top | lx - x < minDiff && i < iMax -> nextState {motion = Running, lastChange = i + 1}
+    Bottom | x < bottomLvl -> nextState {motion = Running}
+    Bottom | x > topLvl -> nextState {motion = Running, border = Top}
+    Bottom | x - lx >= minDiff -> nextState {motion = Running}
+    Bottom | x - lx < minDiff && i < iMax -> nextState {motion = Running, lastChange = i + 1}
+    _ -> nextState {motion = Stopped, border = None}
+  |> \s -> (motion s, s)
+  where
+    nextState = CalcMotionState motion' border' 0 x (max 0 $ lj-1) motionScaler
+    motionScaler = scaler' {lastMotion = if abs (lx - x) > rescaleDiff then 0 else lastMotion' + 1}
+
+    topLvl = zeroLvl' + 3
+    bottomLvl = zeroLvl' - 3
+    topJumpLvl = zeroLvl' + 10
+    bottomJumpLvl = zeroLvl' - 15
+    jumpIgnore' = 5
+    iMax = 4
+    minDiff = 4
+    rescaleAfter = 30
+    rescaleDiff = 1
+calcMotion _ _ = error "invalid position"
